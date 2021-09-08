9c85513993e23cdfa634220b9ff3e3b4c716c4bb
dmuralov <89232600+dmuralov@users.noreply.github.com>
2021-09-08 13:51:53 +0300

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -531,7 +531,7 @@ public:
         m_assumed_blockchain_size = 2;
         m_assumed_chain_state_size = 2;
 
-        genesis = CreateGenesisBlockMainNet(1630663200, 2000, 0x1f00ffff); 
+        genesis = CreateGenesisBlockMainNet(1630663200, 3061400, 0x1f00ffff); 
         consensus.hashGenesisBlock = genesis.GetHash();
         
         std::cout << "hsh -> " << consensus.hashGenesisBlock.ToString() << std::endl;
@@ -739,7 +739,7 @@ public:
         m_assumed_blockchain_size = 1;
         m_assumed_chain_state_size = 1;
 
-        genesis = CreateGenesisBlockTestNet(1630663200, 2000, 0x1f00ffff);
+        genesis = CreateGenesisBlockTestNet(1630663200, 3061400, 0x1f00ffff);
         consensus.hashGenesisBlock = genesis.GetHash();
         
         std::cout << "hsh -> " << consensus.hashGenesisBlock.ToString() << std::endl;
